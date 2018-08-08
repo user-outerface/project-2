c32b5784efc46d401bf58a5df069b2d129f417b4
Francisco760 <f.arechiga@hotmail.com>
2018-08-07 19:16:25 -0600

    google auth1

--- server.js
+++ server.js
@@ -3,9 +3,44 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
 
+
+var passport = require("passport");
+var googleStrategy = require("passport-google-oauth20").Stragey;
+
+// Move to routes.js
+var keys = require("./config/keys");
+
 var db = require("./models");
 
 var app = express();
+
+// Public token 
+// clientID 1006752191890-btp7iod5rnp6ddppkfbnpvq6p9qfv9fk.apps.googleusercontent.com
+
+// Private token
+// clientSecret 9Hyf6L34cC9OVci_gFZac6jS
+
+passport.use(new googleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: "/auth/google/calback"
+  },
+
+  // Testing purposes only.
+  (accessToken, refreshToken, profile, done) => {
+    console.log("access token", accessToken);
+    console.log("refresh token", refreshToken);
+    console.log("profile:", profile);
+  }
+));
+
+
+app.get("/auth/google", passport.authenticate("google", {
+  scope: ["profile", "email"]
+}));
+
+app.get("/auth/google/callback", passport.authenticate("google"));
+
 var PORT = process.env.PORT || 3000;
 
 // Middleware
